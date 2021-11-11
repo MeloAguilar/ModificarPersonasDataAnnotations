11e7642ffe0a487b43bce5efc30d3cc8dbdcecd4
Melo Aguilar <carmelo.aguilar@iesnervion.es>
2021-11-11 12:40:01 +0100

    Agregar archivos de proyecto.

---
+++ ModificarPersonas_Entities/clsPersona.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ModificarPersonas_Entities
+{
+
+    public class clsPersona
+    {
+        [HiddenInput(DisplayValue = false) ]
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Campo Nombre obligatorio")]
+        [StringLength(30, MinimumLength = 3)]
+        public string nombre { get; set; }
+
+        [Required(ErrorMessage = "Campo Apellidos Obligatorio")]
+        [StringLength(40, MinimumLength = 3)]
+        public string apellidos { get; set; }
+
+        [DataType(DataType.Date, ErrorMessage = "La fecha debe introducirse")]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}",ApplyFormatInEditMode = true)]
+        public DateTime fechaNac { get; set; }
+
+        [RegularExpression("[0-9]{3}[0-9]{3}[0-9]{3}", ErrorMessage = "El número de telefono tiene que ser de tipo XXXXXXXXX")]
+        
+        public string telefono { get; set; }
+
+
+        //Constructores
+
+        public clsPersona(int id, string nombre, string apellidos, DateTime fechaNac, string telefono)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.telefono = telefono;
+            this.fechaNac = fechaNac;
+        }
+
+        public clsPersona(string _nombre, string _apellidos)
+        {
+            this.nombre = _nombre;
+            this.apellidos = _apellidos;
+        }
+
+        public clsPersona()
+        {
+
+        }
+
+
+
+        public int GetEdad()
+        {
+            var hoy = DateTime.Now;
+            var edad = hoy.Year - fechaNac.Year;
+            if (fechaNac.AddYears(edad) > hoy)
+            {
+                edad--;
+            }
+            return edad;
+        }
+
+    }
+}
---
+++ ModificarPersonas_UI/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ModificarPersonas_Entities;
+
+namespace ModificarPersonas_UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+           
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index([Bind("id,nombre,apellidos,fechaNac,telefono")] clsPersona persona)
+        {
+            if (!ModelState.IsValid)
+            {
+
+                return View(nameof(Index));
+
+            }
+            else
+            {
+
+                ViewBag.Persona = persona;
+                return View("PersonaModificada");
+
+            }
+
+
+        }
+    }
+
+
+}
